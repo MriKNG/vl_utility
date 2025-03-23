b9db04316f682ab7f414acd8025c471b9135c75d
KNG <kng@playliberty.com>
2025-03-23 16:36:57 +0100

    Update fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+name 'vl_spawner'
+description 'A resource for spawning peds / props and vehicles into your FiveM server world'
+
+author 'KNG'
+repository 'https://github.com/MriKNG/vl_spawner'
+
+shared_scripts {
+    'shared/*.lua',
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+}
+
+dependencies {
+}
+
+files {
+}
+
+lua54 'yes'
